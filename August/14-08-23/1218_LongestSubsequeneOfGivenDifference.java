7562fc75337341d5174bddc599086c0ae9fffd64
Subham Raj <glorious10info@gmail.com>
2023-08-14 21:02:56 +0530

    Comments added in 1218

--- August/14-08-23/1218_LongestSubsequeneOfGivenDifference.java
+++ August/14-08-23/1218_LongestSubsequeneOfGivenDifference.java
@@ -4,9 +4,11 @@ public class 1218_LongestSubsequeneOfGivenDifference {
         
         int n = arr.length;
         int num[] = new int[n];
+        //Making a different array for storing occurences insures to find longest AP among same elements also.
         int max = 0;
 
         Map<Integer, Integer> map = new HashMap<>();
+        //Using HashMap we can map a number with its index and find whether it has occured already while we are traversing backwards.
 
         for(int i = n-1; i >= 0; i--)
         {
