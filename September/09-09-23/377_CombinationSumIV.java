f27dff5e80daeaabcfb551ae25c0cb4eed5317da
Subham Raj <glorious10info@gmail.com>
2023-09-09 22:08:48 +0530

    Combination Sum IV Added

---
+++ September/09-09-23/377_CombinationSumIV.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        
+        int dp[] = new int[target+1];
+        Arrays.fill(dp, -1);
+
+        return solve(nums, target, dp);
+
+    }
+
+    public int solve(int nums[], int target, int dp[])
+    {
+        if(target < 0)return 0;
+        if(target == 0)return 1;
+
+        if(dp[target] != -1)return dp[target];
+
+        int ans = 0;
+        for(Integer num: nums)
+        {
+            if(num <= target)
+            {
+                ans += solve(nums, target-num, dp);
+            }
+        }
+
+        return dp[target]=ans;
+
+    }
+
+}
